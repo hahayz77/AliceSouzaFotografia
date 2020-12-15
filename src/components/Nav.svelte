66302ecaff594e30cd028438c1dad6d340b7c1a0
hahayz77 <danilo__danilo@hotmail.com>
2020-12-15 11:08:52 -0300

    navBar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,13 +3,18 @@
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-trans">
-	<a class="navbar-brand col-lg-2" href="."><img src="img/logo.png" alt=""></a>
-	<ul class="navbar-nav col-lg-10 justify-content-end">
-		<li class="nav-item"><a class="nav-link mx-3" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
-		<li class="nav-item"><a class="nav-link mx-3" aria-current="{segment === 'sobre' ? 'page' : undefined}" href="sobre">Sobre</a></li>
-		<li class="nav-item"><a class="nav-link mx-3" aria-current="{segment === 'portfolio' ? 'page' : undefined}" href="portfolio">Portfólio</a></li>
-		<li class="nav-item"><a class="nav-link mx-3" aria-current="{segment === 'contato' ? 'page' : undefined}" href="contato">Contato</a></li>
-	</ul>
+	<a id="extraBrand" class="navbar-brand col-lg-2" href="."><img src="img/logo.png" alt=""></a>
+	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		<ul class="navbar-nav col-lg-12 px-0 justify-content-end">
+			<li class="nav-item" data-toggle="collapse" data-target="#navbarTogglerDemo01"><a class="nav-link mx-3" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
+			<li class="nav-item" data-toggle="collapse" data-target="#navbarTogglerDemo01"><a class="nav-link mx-3" aria-current="{segment === 'sobre' ? 'page' : undefined}" href="sobre">Sobre</a></li>
+			<li class="nav-item" data-toggle="collapse" data-target="#navbarTogglerDemo01"><a class="nav-link mx-3" aria-current="{segment === 'portfolio' ? 'page' : undefined}" href="portfolio">Portfólio</a></li>
+			<li class="nav-item" data-toggle="collapse" data-target="#navbarTogglerDemo01"><a class="nav-link mx-3" aria-current="{segment === 'contato' ? 'page' : undefined}" href="contato">Contato</a></li>
+		</ul>
+	</div>
 </nav>
 
 
@@ -44,6 +49,22 @@
 	}
 	.bg-trans{
 		background-color: rgba(255, 255, 255, 0.8);
+	}
+
+	@media (max-width: 768px) { 
+		#extraBrand{
+			position: absolute;
+			top: 0;
+			left: 0;
+			text-align: left;
+			margin-top: 0.5rem;
+		}
+		.navbar-toggler{
+			z-index: 99;
+		}
+		.navbar-light .navbar-toggler{
+			border-color: rgba(0,0,0,0);
+		}
 	}
 	
 </style>
--- src/components/portfolio/ToBehance.svelte
+++ src/components/portfolio/ToBehance.svelte
@@ -1,11 +1,12 @@
 <main class="bg-white py-0 my-0">
-        <a href="/portfolio"><button class="d-block mx-auto btn px-5"><span><i class="fa fa-2x fa-behance"></i><p>Ver Mais Albuns no Behance</p></span></button></a>
+        <a href="/portfolio"><button class="d-block mx-auto btn"><span><i class="fa fa-2x fa-behance"></i><p>Ver Mais Albuns no Behance</p></span></button></a>
 </main>
 
 <style>
     button{
         border-radius: 60px;
         background: rgba(255, 189, 205, 0.8);
+        padding: .8rem 2rem;
         transition: all 1000ms;
     }
     button:hover{
@@ -17,5 +18,14 @@
         font-size: 1.5rem;
         text-decoration: none;
     }
+
+    @media (max-width: 576px){
+        i{
+            font-size: 2rem;
+        }
+        button{
+            padding: 0.5rem 1rem;
+        }
+	}
 
 </style>
